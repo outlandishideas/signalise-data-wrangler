e03a5134598e31704e122942520024c338b2741e
Matthew Kendon <matt@outlandish.com>
2021-11-30 17:33:09 +0000

    add monday collector to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 
 from src.collectors.GsuiteCollector import GsuiteCollector
+from src.collectors.MondayCollector import MondayCollector
 from src.util import create_reporting_views, get_reporting_db_engine, prepare_db_for_collection
 
 if __name__ == '__main__':
@@ -9,6 +10,8 @@ if __name__ == '__main__':
     prepare_db_for_collection(db)
     # gsuite = GsuiteCollector(db)
     # gsuite.collect()
-    create_reporting_views(db)
+    monday = MondayCollector(db)
+    monday.collect()
+    # create_reporting_views(db)
 
-    print(db.execute("SELECT * from reporting.events_by_attendee limit 10").fetchall())
+    # print(db.execute("SELECT * from reporting.events_by_attendee limit 10").fetchall())
