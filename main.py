594fb82e1fb6ace25255261ab18efb78d5179176
Matthew Kendon <matt@outlandish.com>
2021-12-07 17:07:41 +0000

    if first day of month use last month otherwise use current month to restrict bookings

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dotenv import load_dotenv
+from datetime import datetime
 
 from src.collectors.GsuiteCollector import GsuiteCollector
 from src.collectors.MondayCollector import MondayCollector
@@ -7,6 +8,7 @@ from src.workers.ExportCCGPerformanceReportWorker import ExportCCGPerformanceRep
 
 
 if __name__ == '__main__':
+    print("Starting")
     load_dotenv()
     db = get_reporting_db_engine()
     prepare_db_for_collection(db)
@@ -17,6 +19,12 @@ if __name__ == '__main__':
     create_reporting_views(db)
 
     worker = ExportCCGPerformanceReportWorker(db)
+    worker.this_month()
+
+    # if its the first day of the month get the last day.
+    if datetime.now().day == 1:
+        worker.last_month()
+
     worker.do_all_work()
 
     # print(db.execute("SELECT * from reporting.events_by_attendee limit 10").fetchall())
