0866f468c704452743d5d2b9883ba9b544882b7f
Matthew Kendon <matt@outlandish.com>
2021-12-15 10:52:32 +0000

    update as to use item_name for booking ref

--- src/reporting_views/01_monday_old_booking_view.sql
+++ src/reporting_views/01_monday_old_booking_view.sql
@@ -5,9 +5,10 @@ SELECT zip_date_hour(booking_date__date, start__hour)                           
      , zip_date_hour(booking_date__date, end__hour9)                                  AS  booking_end_datetime
      , zip_date_hour(deal_closed__date_confirmed8, deal_closed__date_confirmed8)       AS  deal_closed_datetime
      , creation_log__creation_log                                                      AS  request_datetime
-     , sales_contacts__connect_boards5                                                 AS  booker_ref
+     , booking._item_name                                                              AS  booker_ref
+     , sales_contacts__connect_boards5                                                 AS  sales_contact
      , ccg.nacs_code__text4                                                            AS  practice_code
-     , ccg.location__location                                                          AS  location
+     , booking.location__location                                                      AS  location
      , ccg.contract__status                                                             AS  ccg
      , booking_type__session_notes                                                     AS  details_of_request
      , type_of_professional__type_of_professional                                      AS  language_professional
@@ -20,13 +21,18 @@ SELECT zip_date_hour(booking_date__date, start__hour)                           
                 preferred_cp__connect_boards3 = cp_2__connect_boards THEN TRUE
            ELSE FALSE END                                                              AS  named_interpreter_request_met
      , CASE WHEN pref__sex___status0 = 'No preference' THEN FALSE ELSE TRUE END        AS  preferred_sex_of_interpreter_requested
-     , FALSE AS sex_pref_met
+     , CASE
+           WHEN pref__sex___status0 = 'No preference' THEN NULL
+           WHEN pref__sex___status0 = interpreters.sex__status04 THEN TRUE
+           WHEN preferred_interpreters.sex__status04 = interpreters.sex__status04 THEN TRUE
+           ELSE FALSE
+     END AS sex_pref_met
      , (SELECT member__status0
         FROM monday.communication_professional_contacts
         WHERE communication_professional_contacts._item_name = cp_1__connect_boards6)  AS  members_status_to_demo_how_sex_pref_met_should_work
-     , cp_1__connect_boards6                                                           AS  interpreter_ref --todo handle cp2. Could turn this whole view into a unison with exactly the same, but filtered on cp2 - need to know exactly what is desired in the report
-     , invoice_notes__notes                                                                             AS  action_taken_to_investigate_summary
-     , NULL AS  level_of_interpreter_met
+     , interpreters.reg_number__text2                                                           AS  interpreter_ref --todo handle cp2. Could turn this whole view into a unison with exactly the same, but filtered on cp2 - need to know exactly what is desired in the report
+     , invoice_notes__notes                                                            AS  action_taken_to_investigate_summary
+     , NULL AS level_of_interpreter_met
      , CASE
            WHEN charge__status = 'Canc no fee' OR charge__status = 'Canc 50%% fee' OR
                 charge__status = 'Cancelled Full Fee' THEN TRUE
@@ -67,9 +73,14 @@ SELECT zip_date_hour(booking_date__date, start__hour)                           
 FROM monday.booking_sales_pipeline_2020_21 AS booking
 LEFT JOIN monday.sales_contacts AS contacts
     ON contacts._item_name = booking.sales_contacts__connect_boards5
+LEFT JOIN monday.communication_professional_contacts AS interpreters
+    ON interpreters._item_name = booking.cp_1__connect_boards6
+LEFT JOIN monday.communication_professional_contacts AS preferred_interpreters
+    ON preferred_interpreters._item_name = booking.preferred_cp__connect_boards3
 LEFT JOIN monday.ccg_framework_locations AS ccg
     ON contacts.ccg_framework_locations__connect_boards =
        ccg._item_name
+WHERE ccg.contract__status IS NOT NULL
 
 UNION
 
@@ -77,9 +88,10 @@ SELECT zip_date_hour(booking_date__date, start__hour)                           
      , zip_date_hour(booking_date__date, end__hour9)                                  AS  booking_end_datetime
      , zip_date_hour(deal_closed__date_confirmed8, deal_closed__date_confirmed8)       AS  deal_closed_datetime
      , creation_log__creation_log                                                      AS  request_datetime
-     , sales_contacts__connect_boards5                                                 AS  booker_ref
+     , booking._item_name                                                              AS  booker_ref
+     , sales_contacts__connect_boards5                                                 AS  sales_contact
      , ccg.nacs_code__text4                                                            AS  practice_code
-     , ccg.location__location                                                          AS  location
+     , booking.location__location                                                          AS  location
      , ccg.contract__status                                                             AS  ccg
      , booking_type__session_notes                                                     AS  details_of_request
      , type_of_professional__type_of_professional                                      AS  language_professional
@@ -92,11 +104,16 @@ SELECT zip_date_hour(booking_date__date, start__hour)                           
                 preferred_cp__connect_boards3 = cp_2__connect_boards THEN TRUE
            ELSE FALSE END                                                              AS  named_interpreter_request_met
      , CASE WHEN pref__sex___status0 = 'No preference' THEN FALSE ELSE TRUE END        AS  preferred_sex_of_interpreter_requested
-     , FALSE                                                                           AS  sex_pref_met
+     , CASE
+           WHEN pref__sex___status0 = 'No preference' THEN NULL
+           WHEN pref__sex___status0 = interpreters.sex__status04 THEN TRUE
+           WHEN preferred_interpreters.sex__status04 = interpreters.sex__status04 THEN TRUE
+           ELSE FALSE
+     END                                                                         AS  sex_pref_met
      , (SELECT member__status0
         FROM monday.communication_professional_contacts
         WHERE communication_professional_contacts._item_name = cp_2__connect_boards)  AS  members_status_to_demo_how_sex_pref_met_should_work
-     , cp_2__connect_boards                                                           AS  interpreter_ref
+     , interpreters.reg_number__text2                                                           AS  interpreter_ref
      , invoice_notes__notes                                                           AS  action_taken_to_investigate_summary
      , NULL AS  level_of_interpreter_met
      , CASE
@@ -139,8 +156,13 @@ SELECT zip_date_hour(booking_date__date, start__hour)                           
 FROM monday.booking_sales_pipeline_2020_21 AS booking
 LEFT JOIN monday.sales_contacts AS contacts
     ON contacts._item_name = booking.sales_contacts__connect_boards5
+LEFT JOIN monday.communication_professional_contacts AS interpreters
+    ON interpreters._item_name = booking.cp_2__connect_boards
+LEFT JOIN monday.communication_professional_contacts AS preferred_interpreters
+    ON preferred_interpreters._item_name = booking.preferred_cp__connect_boards3
 LEFT JOIN monday.ccg_framework_locations AS ccg
     ON contacts.ccg_framework_locations__connect_boards =
        ccg._item_name
-WHERE cp_2__connect_boards IS NOT NULL
+WHERE ccg.contract__status IS NOT NULL
+AND cp_2__connect_boards IS NOT NULL
 AND cp_2__connect_boards != '';
